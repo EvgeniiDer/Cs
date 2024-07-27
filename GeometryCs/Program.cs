d05581cd52eb25764f98cab96fa60289bd3f58b1
Zymbo <zastava12@yahoo.com>
2024-07-27 12:09:06 +0300

    1.Figures

--- GeometryCs/Program.cs
+++ GeometryCs/Program.cs
@@ -4,7 +4,108 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("0)");
+            for (uint i = 0; i < 5; i++)
+            {
+                for (uint j = 0; j < 5; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            Console.Write("1)");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            Console.WriteLine("2)");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 5; j > i; j--)
+                {
+                    Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+
+            Console.WriteLine("3)");
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 1; j < 6; j++)
+                {
+                    if (j >= i)
+                        Console.Write('*');
+                    else if (j < i)
+                        Console.Write(' ');
+
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            Console.WriteLine("4)");
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 6; j > 0; j--)
+                {
+                    if (j > i)
+                        Console.Write(' ');
+                    else if (j <= i)
+                        Console.Write('*');
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            Console.WriteLine("5)");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = i; j < 6; j++)
+                    Console.Write(' ');
+                Console.Write('/');
+                for (int j = 0; j < i * 2; j++)
+                    Console.Write(' ');
+                Console.Write('\\');
+                Console.Write('\n');
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                    Console.Write(' ');
+                Console.Write('\\');
+                for (int j = (i + 1) * 2; j < (6) * 2; j++)
+                    Console.Write(' ');
+                Console.Write('/');
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+            Console.WriteLine("6)");
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 1; j < 6; j++)
+                {
+                    if ((i % 2) > 0)
+                    {
+                        if ((j % 2) > 0)
+                            Console.Write('+');
+                        else
+                            Console.Write('-');
+                    }
+                    else if ((i % 2) == 0)
+                    {
+                        if ((j % 2) > 0)
+                            Console.Write('-');
+                        else
+                            Console.Write('+');
+                    }
+                }
+                Console.Write('\n');
+            }
+
         }
     }
 }
