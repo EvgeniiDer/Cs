9b5fd6cdb480403eef357f627b97f9635c8788e8
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2024-08-01 17:25:13 +0300

    HomeWork

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -7,8 +7,28 @@ namespace ChessBoard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow World");
-        }
+            int n = 5;
+
+	        for(int a = 0; a < n; a++ )
+	            {
+		            for(int b = 0; b < n; b++)
+		                {
+			                for(int c = 0; c < n; c++)
+				                {
+					                for(int d = 0; d < n; d++)
+					                    {
+						                    if((a + c) % 2 == 0)
+                                                {
+                                                    Console.Write("*");
+                                                }
+                                            else
+                                                Console.Write(" ");
+					                    }		
+				                }
+                                Console.WriteLine();
+		                }
+	            }
+            }
 
     }
 }
