7b55d68ad386477456a2e66538f85c39e577accc
EvgeniiDer <128982133+EvgeniiDer@users.noreply.github.com>
2024-08-01 18:15:23 +0300

    MyHomeWork

---
+++ Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Game{
+  class Program
+    {
+    static int x = 0;
+    static int y = 0;
+
+    static void Main()
+    {
+        Console.CursorVisible = false;
+
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.W)
+                {
+                    y--;
+                }
+                else if (key == ConsoleKey.S)
+                {
+                    y++;
+                }
+                else if (key == ConsoleKey.A)
+                {
+                    x--;
+                }
+                else if (key == ConsoleKey.D)
+                {
+                    x++;
+                }
+
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                Console.Write("■");
+            }
+
+            Thread.Sleep(50);
+        }
+    }
+    }
+}
+
