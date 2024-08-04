aa2b95f8b2287bf51f5638ae4b1ca0c076b12699
Zymbo <zastava12@yahoo.com>
2024-08-04 18:24:42 +0300

    Home Work

---
+++ Calculator_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Calculator_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Expression:");
+            string input = Console.ReadLine();
+
+            var result = new System.Data.DataTable().Compute(input, "");
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
+
+        }
+    }
+}
